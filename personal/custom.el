b0d2c720ce298c937be7912b925ea8b972ffb392
Stefan Strigler <stefan.strigler@erlang-solutions.com>
2013-03-08 09:02:12 +0100

    global autocomoplete is not working so disabled it

--- personal/custom.el
+++ personal/custom.el
@@ -17,13 +17,13 @@
 (fci-mode)
 
 ;; dirty fix for having AC everywhere
-(define-globalized-minor-mode real-global-auto-complete-mode
-  auto-complete-mode (lambda ()
-                       (if (not (minibufferp (current-buffer)))
-                           (auto-complete-mode 1))
-                       ))
-(real-global-auto-complete-mode t)
-(require 'auto-complete)
+;;(define-globalized-minor-mode real-global-auto-complete-mode
+;;  auto-complete-mode (lambda ()
+;;                       (if (not (minibufferp (current-buffer)))
+;;                           (auto-complete-mode 1))
+;;                       ))
+;;(real-global-auto-complete-mode t)
+;;(require 'auto-complete)
 
 
 ;; Erlang
